55dcc1085d091b57a12a7ebc1f065e0c1f024e09
Daniel Ayres <daniel@email.com>
2026-02-26 15:57:26 -0300

    test 6

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -125,7 +125,7 @@ const Login = () => {
                   Entrando...
                 </span>
               ) : (
-                "Entrar"
+                "Acessar Sistema"
               )}
             </Button>
           </form>
